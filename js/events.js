ed044b5c71c5ed2f5265652b16be309a47c1e821
Noah Sragow <nsragow@gmail.com>
2018-12-05 20:29:27 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,46 @@
 $(document).ready(function(){
 
 // call functions here
-
+  console.log("start");
+  getIt();
+  frameIt()
+  pressIt()
+  submitIt()
 });
+
+function getIt()
+{
+  
+  $("p").on("click",function(){
+    
+    alert("Hey!");
+    
+  });
+}
+
+function frameIt()
+{
+  $("img").on("load",function()
+  {
+    $("img").addClass("tasty")
+  })
+}
+
+function pressIt()
+{
+  $(document).on("keydown", function(key)
+  {
+    if(key.which === 71)
+    {
+      alert("youdidit")
+    }
+  })
+}
+
+function submitIt()
+{
+  $("form").on("submit", function()
+  {
+    alert("Your form is going to be submitted now.")
+  })
+}
