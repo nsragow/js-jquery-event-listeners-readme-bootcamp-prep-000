107c13018c3096880fdd33fad42e1fb5bf0415f6
Noah Sragow <nsragow@gmail.com>
2018-12-05 20:22:07 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -3,5 +3,38 @@
 $(document).ready(function(){
 
 // call functions here
-
+  console.log("start");
+  getIt();
+  frameIt()
 });
+
+function getIt()
+{
+  
+  $("p").on("click",function(){
+    
+    alert("Hey!");
+    
+  });
+}
+
+function frameIt()
+{
+  $("img").on("load",function()
+  {
+    $("img").addClass("tasty")
+  })
+}
+
+function pressIt()
+{
+  $(document).on("keydown", function()
+  {
+    if()
+  })
+}
+
+function submitIt()
+{
+  
+}
